5582928c6241822c1eb475b0672fcc25e2d38b07
Veri Anggoro <Veri.anggoro@melon.co.id>
2022-08-09 19:48:51 +0700

    bypass login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -48,71 +48,74 @@ class AuthController extends Controller
         ]);
 
         try {
-            $ch = curl_init();
-            $headers  = [
-                        'Content-Type: application/json',
-                        'Accept: application/json',
-                    ];
-            $postData = [
-                'personal_number' => $request->personal_number,
-                'katasandi' => $request->katasandi,
-                'ip' => $_SERVER['REMOTE_ADDR'],
-            ];
-            curl_setopt($ch, CURLOPT_URL,config('app.url_be').'api/login_web');
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER , false);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            $result     = curl_exec ($ch);
-            $hasil = json_decode($result);
+//            $ch = curl_init();
+//            $headers  = [
+//                        'Content-Type: application/json',
+//                        'Accept: application/json',
+//                    ];
+//            $postData = [
+//                'personal_number' => $request->personal_number,
+//                'katasandi' => $request->katasandi,
+//                'ip' => $_SERVER['REMOTE_ADDR'],
+//            ];
+//            curl_setopt($ch, CURLOPT_URL,config('app.url_be').'api/login_web');
+//            curl_setopt($ch, CURLOPT_POST, 1);
+//            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER , false);
+//            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+//            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//            $result     = curl_exec ($ch);
+//            $hasil = json_decode($result);
 
+            session(['logged_in' => true]);
+            Session::flash('term', true);
 
-        //    dd($hasil);
-            if (isset($hasil->status)) {
-                if ($hasil->status == 1) {
-                    // refresh session
-                    Session::flush();
-                    $temp['id']                 = $hasil->data->id;
-                    $temp['avatar_id']          = $hasil->data->avatar_id;
-                    $temp['exp']                = $hasil->data->exp;
-                    $temp['level_id']           = $hasil->data->level_id;
-                    $temp['level_name']         = $hasil->data->level_name;
-                    $temp['name']               = $hasil->data->name;
-                    $temp['personal_number']    = $hasil->data->personal_number;
-                    $temp['username']           = $hasil->data->username;
-                    $temp['email']              = $hasil->data->email;
-                    $temp['direktorat']         = $hasil->data->direktorat;
-                    $temp['divisi']             = $hasil->data->divisi;
-                    $temp['last_login_at']      = $hasil->data->last_login_at;
-                    $temp['login_at']           = $hasil->data->login_at;
-                    $temp['token_bri']          = $hasil->data->token_bri;
-                    // $temp['token_bri']          = 'dummy';
-                    $temp['token']              = $hasil->data->token;
-                    $temp['role']               = $hasil->data->role;
-                    session($temp);
-
-                }else{
-                    if ($hasil->status == 0 && isset($hasil->data->validator)) {
-                        $validasi = '<ul>';
-                        foreach ($hasil->data->validator as $key => $value) {
-                            $validasi .= "<li>$value[0]</li>";
-                        }
-                        $validasi .= '</ul>';
-
-                        Session::flash('error', $validasi);
-                    }elseif ($this->long_many_time_login() == true) {
-                        Session::flash('error', 'Terlalu Sering Melakukan Kesalahan Saat Login');
-                    }else{
-                        Session::flash('error', $hasil->data->message);
-                    }
-
-                    return back();
-                }
-            }else{
-                Session::flash('error','Something Problem');
-                return back();
-            }
+            return redirect('/');
+//
+//        //    dd($hasil);
+//            if (isset($hasil->status)) {
+//                if ($hasil->status == 1) {
+//                    // refresh session
+//                    Session::flush();
+//                    $temp['id']                 = $hasil->data->id;
+//                    $temp['avatar_id']          = $hasil->data->avatar_id;
+//                    $temp['exp']                = $hasil->data->exp;
+//                    $temp['level_id']           = $hasil->data->level_id;
+//                    $temp['level_name']         = $hasil->data->level_name;
+//                    $temp['name']               = $hasil->data->name;
+//                    $temp['personal_number']    = $hasil->data->personal_number;
+//                    $temp['username']           = $hasil->data->username;
+//                    $temp['email']              = $hasil->data->email;
+//                    $temp['direktorat']         = $hasil->data->direktorat;
+//                    $temp['divisi']             = $hasil->data->divisi;
+//                    $temp['last_login_at']      = $hasil->data->last_login_at;
+//                    $temp['login_at']           = $hasil->data->login_at;
+//                    $temp['token_bri']          = $hasil->data->token_bri;
+//                    // $temp['token_bri']          = 'dummy';
+//                    $temp['token']              = $hasil->data->token;
+//                    $temp['role']               = $hasil->data->role;
+//                    session($temp);
+//                }else{
+//                    if ($hasil->status == 0 && isset($hasil->data->validator)) {
+//                        $validasi = '<ul>';
+//                        foreach ($hasil->data->validator as $key => $value) {
+//                            $validasi .= "<li>$value[0]</li>";
+//                        }
+//                        $validasi .= '</ul>';
+//
+//                        Session::flash('error', $validasi);
+//                    }elseif ($this->long_many_time_login() == true) {
+//                        Session::flash('error', 'Terlalu Sering Melakukan Kesalahan Saat Login');
+//                    }else{
+//                        Session::flash('error', $hasil->data->message);
+//                    }
+//
+//                    return back();
+//                }
+//            }else{
+//                Session::flash('error','Something Problem');
+//                return back();
+//            }
         }catch (\Throwable $th) {
             Session::flash('error',"Something Error, Try Again Please".$th);
             return back();
