b742978b08ed3cf8b6d27ac38d8cc092cc2f1598
Veri Anggoro <Veri.anggoro@melon.co.id>
2022-09-28 15:39:30 +0700

    update lesson learned

--- resources/views/mylessonlearned.blade.php
+++ resources/views/mylessonlearned.blade.php
@@ -18,160 +18,154 @@
 @section('back', route('home'))
 
 @section('content')
-    <div class="row judul">
-        <div class="col-md-12 px-0 header-detail">
-            <div class="row px-2">
-                <div class="col md-12">
-                    @if (session('role') != 0)
-                        <h3>Pending Request</h3>
-                    @else
-                        <h3>My Project</h3>
-                        <div class="d-flex bd-highlight">
-                            <div class="mr-auto p-2 bd-highlight">
-                                <div class="p-2 bd-highlight" id="drop-nama-proyek">
-                                    <div class="dropdown show">
-                                        <a class="btn btn-outline-secondary bg-white text-black-50 btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                           data-toggle="modal" data-target="#modal-sort-nama" aria-haspopup="true" aria-expanded="false">
-                                            Tahap Project
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Dropdowns TEMP -->
+    <div class="row">
+        <div class="col-md-12" id="konten">
+            <h3 class="pl-2 pt-5">Manage Lesson Learned</h3>
+            <div class="d-flex justify-content-start mb-3 px-3">
+                <div class="p-2 bd-highlight" id="drop-nama-proyek">
+                    <div class="dropdown show">
+                        <a class="btn btn-outline-secondary bg-white text-black-50 btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                           data-toggle="modal" data-target="#modal-sort-nama" aria-haspopup="true" aria-expanded="false">
+                            Tahap Project
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- NAVIGASI -->
+            <div class="d-flex bd-highlight">
+                <div class="mr-auto p-2 bd-highlight">
+                    <h4>Development</h4>
+                </div>
+                <!-- Dropdowns TEMP -->
 
-                            <!-- Dropdowns TEMP -->
+                <!-- Dropdowns TEMP -->
 
-                            <!-- Dropdowns -->
-                            <div class="p-2 bd-highlight" id="drop-nama-proyek">
-                                <div class="dropdown show">
-                                    <a class="btn btn-outline-secondary bg-white text-black-50 btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                       data-toggle="modal" data-target="#modal-sort-nama" aria-haspopup="true" aria-expanded="false">
-                                        Direktorat
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="p-2 bd-highlight" id="drop-pemilik">
-                                <div class="dropdown show">
-                                    <a class="btn btn-outline-secondary bg-white text-black-50 btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                       data-toggle="modal" data-target="#modal-sort-pemilik" aria-haspopup="true" aria-expanded="false">
-                                        Unit Kerja
-                                    </a>
-                                </div>
-                            </div>
-                            <!-- Dropdowns -->
+                <!-- Dropdowns -->
+                <div class="p-2 bd-highlight" id="drop-nama-proyek">
+                    <div class="dropdown show">
+                        <a class="btn btn-outline-secondary bg-white text-black-50 btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                           data-toggle="modal" data-target="#modal-sort-nama" aria-haspopup="true" aria-expanded="false">
+                            Direktorat
+                        </a>
+                    </div>
+                </div>
+                <div class="p-2 bd-highlight" id="drop-pemilik">
+                    <div class="dropdown show">
+                        <a class="btn btn-outline-secondary bg-white text-black-50 btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                           data-toggle="modal" data-target="#modal-sort-pemilik" aria-haspopup="true" aria-expanded="false">
+                            Unit Kerja
+                        </a>
+                    </div>
+                </div>
+                <!-- Dropdowns -->
 
-                            <!-- Search -->
-                            <div class="p-2 bd-highlight" id="search">
-                                <form action="#" class="w-100" id="search">
-                                    <div class="input-group input-group-sm mb-3">
-                                        <div class="input-group-prepend">
+                <!-- Search -->
+                <div class="p-2 bd-highlight" id="search">
+                    <form action="#" class="w-100" id="search">
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="input-group-prepend">
                                     <span class="input-group-text" id="search">
                                         <i class="fa fa-search fa-sm" aria-hidden="true"></i>
                                     </span>
-                                        </div>
-                                        <input type="text" class="form-control form-control-sm" placeholder="Search Lesson..." aria-describedby="inputGroup-sizing-sm">
-                                    </div>
-                                </form>
                             </div>
-                            <!-- Search -->
+                            <input type="text" class="form-control form-control-sm" placeholder="Search Lesson..." aria-describedby="inputGroup-sizing-sm">
                         </div>
-                    @endif
+                    </form>
                 </div>
-                @if (Session::has('success'))
-                    <div class="col-md-12">
-                        <div class="alert alert-success alert-dismissible show fade pb-1">
-                            <div class="alert-body mb-2">
-                                <button class="close" data-dismiss="alert">
-                                    <span>×</span>
-                                </button>
-                                <small>
-                                    {!!session()->get('success')!!}
-                                </small>
-                            </div>
-                        </div>
-                    </div>
-                @endif
-                @if (Session::has('error'))
-                    <div class="col-md-12">
-                        <div class="alert alert-danger alert-dismissible show fade pb-1">
-                            <div class="alert-body mb-2">
-                                <button class="close" data-dismiss="alert">
-                                    <span>×</span>
-                                </button>
-                                <small>
-                                    {!!session()->get('error')!!}
-                                </small>
-                            </div>
-                        </div>
-                    </div>
-                @endif
-                @if (!empty($data))
-                    <div class="table-responsive">
-                        <table class="table table-main">
-                            <thead class="thead-light">
-                            <tr>
-                                <th style="border-left: 1px solid rgb(245 245 245); border-top-left-radius: 12px;">Direktorat</th>
-                                <th style="width: 200px;">Unit Kerja</th>
-                                <th>Nama Project</th>
-                                <th>Konsultan/Vendor</th>
-                                <th>Lesson Learned</th>
-                                <th>Keterangan</th>
-                                <th style="border-right: 1px solid rgb(245 245 245); border-top-right-radius: 12px;">Action</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @php
-                                $i = ($data->current_page * 10)-9;
-                            @endphp
-                            @forelse ($data->data as $item)
-                                <tr class="py-2">
-                                    <td class="corner-left">{{$item->direktorat}}</td>
-                                    <td>{{$item->divisi}}</td>
-                                    <td>{{$item->project_name}}</td>
-                                    <td>{{$item->consultant_name}}</td>
-                                    <td>{{$item->lesson_learned}}</td>
-                                    <td>{{$item->detail}}</td>
-                                    <td class="corner-right">
-                                        <div class="row">
-                                            <div class="col-6">
-                                                <button class="btn dropdown-item">
-                                                    <i class="fa fa-share-alt mr-2"></i>
-                                                </button>
-                                            </div>
-                                            <div class="col-6">
-                                                <button class="btn dropdown-item">
-                                                    <i class="fa fa-star mr-2"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @empty
-                                <tr class="py-2">
-                                    <td class="corner-left corner-right" colspan="9">Tidak ada data</td>
-                                </tr>
-                            @endforelse
-                            </tbody>
-                        </table>
-                    </div>
-                @elseif(!Session::has('error'))
-                    <div class="col-md-12 pt-2">
-                        <span>Anda belum memiliki project.</span>
-                    </div>
-                @endif
+                <!-- Search -->
             </div>
-        </div>
-    </div>
-    <div class="modal fade bd-example-modal-lg modal-preview" id="modalpreview" tabindex="-1" role="dialog" aria-labelledby="preview" aria-hidden="true">
-        <div class="modal-dialog modal-lg modal-dialog-centered dialog-preview" role="document">
-            <div class="modal-content content-preview bg-transparent">
-                <div class="w-100 d-flex justify-content-center align-items-center" id="content-preview">
-                    <div class="bg-white bg-white w-100 content-preview">
+{{--            <div class="d-flex justify-content-end mb-3 px-3">--}}
+{{--                <div>--}}
+{{--                    Sync Elastic :--}}
+{{--                    @if($sync_es == 0)--}}
+{{--                        <span class="text-success">Done</span>--}}
+{{--                    @else--}}
+{{--                        <span class="text-warning">--}}
+{{--                    <i class="fa fa-sync fa-spin mr-1" aria-hidden="true" style="font-size:12px"></i>--}}
+{{--                    --}}{{-- {{ $sync_es }} Project Remaining --}}
+{{--                    Processing . .--}}
+{{--                  </span>--}}
+{{--                    @endif--}}
+{{--                </div>--}}
+{{--            </div>--}}
+            <!-- NAVIGASI -->
+
+            @include('layouts.alert')
+
+            <!-- REVIEW -->
+            <div class="table-responsive" id="review">
+                <div class="card card-body w-100 d-flex mb-4" style="border-radius: 10px">
+                    <div class="row">
+                        <div class="col-2">
+                            <h6>Direktorat</h6>
+                        </div>
+                        <div class="col-3">
+                            <h6>Uker</h6>
+                        </div>
+                        <div class="col-3">
+                            <h6>Nama Project</h6>
+                        </div>
+                        <div class="col-2">
+                            <h6>Konsultan</h6>
+                        </div>
+                        <div class="col-2">
+                            <h6>Action</h6>
+                        </div>
                     </div>
                 </div>
+                {{--                for each--}}
+                <div class="card card-body w-100 d-flex mb-1" style="border-radius: 10px">
+                    <div class="row">
+                        <div class="col-2">
+                            <p class="text-primary">Satuan Kerja Audit Intern</p>
+                        </div>
+                        <div class="col-3">
+                            <p class="text-primary">Micro Business Development Division</p>
+                        </div>
+                        <div class="col-3">
+                            <p>Fraud Risk Indicator Framework</p>
+                        </div>
+                        <div class="col-2">
+                            <p class="text-primary">Deloitte</p>
+                        </div>
+                        <div class="col-2">
+                            <button class="btn btn-outline-secondary fas fa-pen"></button>
+                            <button class="btn btn-outline-secondary fas fa-trash"></button>
+                            <button class="btn btn-outline-secondary fas fa-arrow-down" data-toggle="collapse" data-target="#collapsData" aria-expanded="false" aria-controls="collapsData"></button>
+                        </div>
+                    </div>
+                    <div class="collapse" id="collapsData">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-4">
+                                    <h6>Lesson Learned</h6>
+                                </div>
+                                <div class="col-8">
+                                    <h6>Keterangan</h6>
+                                </div>
+                            </div>
+                            <hr/>
+                            {{--                            for each--}}
+                            <div class="row">
+                                <div class="col-4">
+                                    <p>Pemulihan Data</p>
+                                </div>
+                                <div class="col-8">
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                </div>
+                            </div>
+                            <hr/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-sm-end content-pagination" id="pag">
+                </div>
             </div>
+            <!-- REVIEW -->
         </div>
     </div>
+
 @endsection
 
 @push('page-script')
